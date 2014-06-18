2dd0ca6344911e54869ae417242ef6e269d90852
Markus Hauck <markus1189@gmail.com>
2014-06-18 22:41:41 +0200

    IRC monad

--- Main.hs
+++ Main.hs
@@ -1,16 +1,24 @@
 {-# LANGUAGE OverloadedStrings, PatternSynonyms, ViewPatterns #-}
-import           Data.Char (toLower)
+
 import           Control.Monad (forever)
+import           Control.Monad.IO.Class (liftIO)
+import           Control.Monad.Reader (ReaderT, ask, runReaderT)
 import qualified Data.ByteString.Char8 as B
+import           Data.Char (toLower)
 import           Network
-import           Network.IRC.Parser (decode)
-import           Network.IRC.Base ( Message(Message, msg_prefix)
+import           Network.IRC.Base ( Message(Message)
                                   , Prefix(NickName)
                                   )
-import           System.IO
+import           Network.IRC.Parser (decode)
 import           System.Exit
-import           System.Random (randomIO)
+import           System.IO
 import           System.Process
+import           System.Random (randomIO)
+
+type IRC a = ReaderT Handle IO a
+
+runIRC :: Handle -> IRC a -> IO a
+runIRC h m = runReaderT m h
 
 pattern PING s <- Message _ "PING" [s]
 pattern JOIN c <- Message _ "JOIN" [c]
@@ -21,14 +29,26 @@ pattern SAY_HELLO c <- COMMAND c (map toLower -> "say hello")
 pattern SAY_TIME c <- COMMAND c (map toLower -> "what time is it?")
 pattern LEAVE c <- COMMAND c (map toLower -> "get lost")
 
-nick :: Handle -> String -> IO ()
-nick h name = hPutStrLn h ("NICK " ++ name)
+pattern FROM_MASTER <- Message (Just (NickName (isMaster -> True) _ _)) _ _
 
-user :: Handle -> String -> IO ()
-user h name = hPutStrLn h ("USER " ++ name ++ " 0 * :" ++ name)
+ircPutStrLn :: B.ByteString -> IRC ()
+ircPutStrLn s = do
+  h <- ask
+  liftIO $ B.hPutStrLn h s
 
-joinChan :: Handle -> String -> IO ()
-joinChan h c = hPutStrLn h ("JOIN " ++ c)
+ircGetLine :: IRC B.ByteString
+ircGetLine = do
+  h <- ask
+  liftIO $ B.hGetLine h
+
+nick :: B.ByteString -> IRC ()
+nick = ircPutStrLn . B.append "NICK "
+
+user :: B.ByteString -> IRC ()
+user name = ircPutStrLn . B.concat $ ["USER ", name, " 0 * :", name]
+
+joinChan :: B.ByteString -> IRC ()
+joinChan = ircPutStrLn . B.append "JOIN "
 
 main :: IO ()
 main = do
@@ -38,34 +58,43 @@ main = do
 
   rndNumber <- randomIO :: IO Int
 
-  nick h $ "IRCBot" ++ show rndNumber
-  user h $ "IRCBot" ++ show rndNumber
+  runIRC h $ do
+    nick $ B.append "IRCBot" (B.pack . show $ rndNumber)
+    user $ B.append "IRCBot" (B.pack . show $ rndNumber)
 
-  joinChan h "##os2-lab"
+    joinChan "##os2-lab"
 
-  forever (loop h)
+    forever loop
 
-sendMsg :: Handle -> B.ByteString -> String -> IO ()
-sendMsg h c s = B.hPutStrLn h (B.pack $ "PRIVMSG " ++ B.unpack c ++ " :" ++ s)
+sendMsg :: B.ByteString -> B.ByteString -> IRC ()
+sendMsg c s = ircPutStrLn . B.concat $ ["PRIVMSG ", c, " :",s]
 
 isMaster :: B.ByteString -> Bool
 isMaster s = s == "predator117" || s == "predator217"
 
-loop :: Handle -> IO ()
-loop h = do
-  line <- B.hGetLine h
+loop :: IRC ()
+loop = do
+  line <- ircGetLine
   flip (maybe $ return ()) (decode line) $ \msg ->
-    case msg_prefix msg of
-      Just (NickName (isMaster -> True) _ _) ->
-        case msg of
-          SAY_HELLO c -> sendMsg h c "Hello everybody."
-          SAY_TIME c -> readProcess "date" [] [] >>= sendMsg h c
-          LEAVE _ -> do
-            B.hPutStrLn h . B.pack $ "QUIT :It was a pleasure."
-            exitSuccess
-          _ -> B.putStr "Don't know what about: " >> B.putStrLn line
-      _ -> case msg of
-        PING s -> B.hPutStrLn h . B.pack $ "PONG :" ++ B.unpack s
-        JOIN c -> sendMsg h c "Your faithful servant awaits commands."
-        COMMAND c _ -> sendMsg h c "You are not my master."
-        _ -> B.putStr "Don't know what about: " >> B.putStrLn line
+    case msg of
+      FROM_MASTER -> handleMasterMsg msg line
+      _ -> handleMsg msg line
+
+handleMasterMsg :: Message -> B.ByteString -> IRC ()
+handleMasterMsg msg line = case msg of
+  SAY_HELLO c -> sendMsg c "Hello everybody."
+  SAY_TIME c -> (liftIO $ readProcess "date" [] []) >>= (sendMsg c . B.pack)
+  LEAVE _ -> do
+    ircPutStrLn "QUIT :It was a pleasure."
+    liftIO exitSuccess
+  _ -> liftIO $ B.putStr "Don't know what about: " >> B.putStrLn line
+
+handleMsg :: Message -> B.ByteString -> IRC ()
+handleMsg msg line = case msg of
+  PING s -> pong s
+  JOIN c -> sendMsg c "Your faithful servant awaits commands."
+  COMMAND c _ -> sendMsg c "You are not my master."
+  _ -> liftIO $ B.putStr "Don't know what to do with: " >> B.putStrLn line
+
+pong :: B.ByteString -> IRC ()
+pong = ircPutStrLn . B.append "PONG :"
